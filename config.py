0f2eb90c46b15194e04002de3a6ad5ad548a9c6d
TCRONEB HACKX <thelastcroneb@gmail.com>
2025-04-12 19:56:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    BOT_TOKEN = "" # 🌚 get it from @botfather (telegram) by sending /newbot command.
-    API_ID =  # 🌚 Same as APP_ID. Get it from my.telegram.org.
-    API_HASH = "" # 🌚 Get it from my.telegram.org.
+    BOT_TOKEN = "8158102163:AAE9pDFuDSK1MA4hfosXPoovj6UcMFFNReA" # 🌚 get it from @botfather (telegram) by sending /newbot command.
+    API_ID =  "24413166"#🌚 Same as APP_ID. Get it from my.telegram.org.
+    API_HASH = "7d43dfedf3baa98292adb7b3f69cd6bd" # 🌚 Get it from my.telegram.org.
     
